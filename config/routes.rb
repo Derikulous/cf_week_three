fc00fc56ad3940e214f9991a4fea3582e1fcc99a
Derik Strattan <derik.strattan@gmail.com>
2013-09-01 22:43:22 -0700

    Welcome pages/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CfWeekThree::Application.routes.draw do
+get '/welcome', to: "pages#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
